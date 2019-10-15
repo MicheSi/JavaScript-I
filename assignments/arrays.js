970454e7cae032889ab83012e3fa282cd52719a8
mlazzell17@gmail.com <MicheSi>
2019-10-15 07:47:28 -0700

    removed code line 103 from arrays - redundant

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -100,7 +100,7 @@ console.log(carModels);
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
 for (let i = 0; i < inventory.length; i++) {
-  if (inventory[i].car_year === inventory[i].car_year)
+  // if (inventory[i].car_year === inventory[i].car_year)
   carYears.push(inventory[i].car_year);
 }
 
